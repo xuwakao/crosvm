31d6153b6fe40bfb0e99a51a5d8727ec8e1cc4ab
xuwakao <xuwakao@gmail.com>
2026-04-04 21:08:29 +0800

    fix: enable use_vulkan for gfxstream mode on macOS

--- src/crosvm/sys/macos/mod.rs
+++ src/crosvm/sys/macos/mod.rs
@@ -448,6 +448,7 @@ pub fn run_config(cfg: Config) -> Result<ExitState> {
             {
                 use devices::virtio::gpu::GpuMode;
                 gpu_params.mode = GpuMode::ModeGfxstream;
+                gpu_params.use_vulkan = Some(true);
             }
             #[cfg(all(feature = "virgl_renderer", not(feature = "gfxstream")))]
             {
